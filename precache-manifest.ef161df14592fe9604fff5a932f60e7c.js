db5cc999b5f07fa3459bd3c0b40b937c971c3336
bumsuk980119 <beom.kang17@gmail.com>
2020-06-30 23:52:47 +0900

    Updates

--- precache-manifest.ef161df14592fe9604fff5a932f60e7c.js
+++ precache-manifest.ef161df14592fe9604fff5a932f60e7c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1601369bf6786e88653f2e84d2da295c",
+    "revision": "cb7314bdd4bc890e8cc3fbf3e07e3bf2",
     "url": "/naver-redesign/index.html"
   },
   {
-    "revision": "be33e1d619455e7e69f2",
+    "revision": "1ab185d0ed6e6b60d0bf",
     "url": "/naver-redesign/static/css/main.816d6bfd.chunk.css"
   },
   {
-    "revision": "37be67caa2461f1eb42b",
-    "url": "/naver-redesign/static/js/2.b10848ee.chunk.js"
+    "revision": "233b0ed79be5ada78923",
+    "url": "/naver-redesign/static/js/2.93a074b4.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/naver-redesign/static/js/2.b10848ee.chunk.js.LICENSE.txt"
+    "url": "/naver-redesign/static/js/2.93a074b4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "be33e1d619455e7e69f2",
-    "url": "/naver-redesign/static/js/main.0558b340.chunk.js"
+    "revision": "1ab185d0ed6e6b60d0bf",
+    "url": "/naver-redesign/static/js/main.3e94cadf.chunk.js"
   },
   {
     "revision": "4c9181d2e8b3bb862889",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/naver-redesign/precache-manifest.5a02c4f288097a17d74492498e381c65.js"
+  "/naver-redesign/precache-manifest.ef161df14592fe9604fff5a932f60e7c.js"
 );
 
 self.addEventListener('message', (event) => {
