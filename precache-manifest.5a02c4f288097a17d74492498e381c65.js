8b9f8cac4ad9e174ded84f75987a4d503e24df45
bumsuk980119 <beom.kang17@gmail.com>
2020-06-30 21:52:11 +0900

    Updates

---
+++ precache-manifest.5a02c4f288097a17d74492498e381c65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1601369bf6786e88653f2e84d2da295c",
+    "url": "/naver-redesign/index.html"
+  },
+  {
+    "revision": "be33e1d619455e7e69f2",
+    "url": "/naver-redesign/static/css/main.816d6bfd.chunk.css"
+  },
+  {
+    "revision": "37be67caa2461f1eb42b",
+    "url": "/naver-redesign/static/js/2.b10848ee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/naver-redesign/static/js/2.b10848ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be33e1d619455e7e69f2",
+    "url": "/naver-redesign/static/js/main.0558b340.chunk.js"
+  },
+  {
+    "revision": "4c9181d2e8b3bb862889",
+    "url": "/naver-redesign/static/js/runtime-main.17c374a0.js"
+  },
+  {
+    "revision": "72c946ce6b1f7c10ce2c3a7360167cd2",
+    "url": "/naver-redesign/static/media/logo.72c946ce.png"
+  }
+]);
